ce2cd40cdadefbadb30c378f4d3ea11d978d81e4
Tom Sherman <the.tomsherman@gmail.com>
2019-01-31 22:08:48 +0000

    Add Session#start

--- lib/Session.ts
+++ lib/Session.ts
@@ -1,31 +1,36 @@
-import { RestClient, IRestResponse } from 'typed-rest-client/RestClient'
+import { RestClient, IRestResponse } from 'typed-rest-client/RestClient';
+import { BasicCredentialHandler } from 'typed-rest-client/Handlers';
 
 export class Session {
   private rest: RestClient;
-  readonly key: string;
   readonly contextId?: string;
   readonly kmId: string | null;
 
   constructor (options: string | SessionOptions) {
     let baseUrl: string;
+    let apiKey: string;
 
     if (typeof options === 'string') {
-      this.key = options;
+      apiKey = options;
       baseUrl = Session.DEFAULT_BASE_URL;
     } else {
-      this.key = options.key;
+      apiKey = options.key;
       baseUrl = options.baseUrl;
       this.contextId = options.contextId;
     }
 
     this.kmId = null
-    this.rest = new RestClient('rainbird-api-js', baseUrl);
+
+    const creds = new BasicCredentialHandler(apiKey, '');
+    this.rest = new RestClient('rainbird-api-js', baseUrl, [ creds ]);
   }
 
-  async start (kmId?: string) {
+  async start (kmId?: string): Promise<IRestResponse<StartResponse>> {
     if (typeof kmId === 'undefined' && this.kmId === null) {
       throw new Error();
     }
+
+    return this.rest.get<StartResponse>('/start')
   }
 
   static DEFAULT_BASE_URL = 'https://api.rainbird.ai';
@@ -36,4 +41,11 @@ export interface SessionOptions {
   baseUrl: string;
   contextId?: string;
   kmId?: string;
+}
+
+export interface StartResponse {
+  id: string;
+  kmVersion: {
+    id: string;
+  };
 }
